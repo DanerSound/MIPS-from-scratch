7a920d649f18a58f2d2722c7e49ac1a78bae8866
Andre Cristhian <andre.barreto28@gmail.com>
2019-03-03 00:18:01 +0100

    added while statement and 1 exe that use it

--- addLoadStore/storeWord.asm
+++ addLoadStore/storeWord.asm
@@ -6,11 +6,11 @@ array:  .word 2, 4, 5, 9, 133, 44, 1, 0
 .text
 main:
 
-    li    $t5, 4          # define index
+    li    $t5, 4          # define an index
     add   $t5, $t5, $t5   # calculate correct offset
-    lw    $s0, array($t5)
+    lw    $s0, array($t5) # load the value from array
+    
     li    $s1, 4           # read a ramdom constant
-
     add   $t3, $s0,$s1
 
     sw    $t3, array($t5)
