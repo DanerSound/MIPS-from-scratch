8478f055e19d945fccb81a335f835bff138fde73
Andre Cristhian <andre.barreto28@gmail.com>
2019-03-03 00:44:40 +0100

    added for statement

---
+++ controlFlow/for_statement.asm
@@ -0,0 +1,15 @@
+# simple workflow to understand the for statement
+# for(index=0; index<= n; i++) 	
+#
+.data
+.text 
+main:
+	move $t0, $zero	# force index to zero	
+	li   $t1, 5	# load the max size 
+
+for:	blt  $t1, $t0, exit
+	add  $t0, $t0, 1
+	j    for
+	
+exit:	li   $v0,10
+	syscall				
